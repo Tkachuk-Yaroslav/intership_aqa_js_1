cea86b1add9f2b6470e4bff539eb094e2526ef95
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-08-17 00:50:05 +0300

    test case 2(needs to be completed)

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -5,40 +5,7 @@
 // });
 
 describe("The Home Page", () => {
-  it("Sign up to Telnyx with not valid email", () => {
-    cy.on("uncaught:exception", (err, runnable) => {
-      // поверни false, щоб Cypress не зупиняв тест при виникненні цієї помилки
-      if (
-        err.message.includes(
-          "The fetching process for the media resource was aborted"
-        )
-      ) {
-        return false;
-      }
-    });
-
-    cy.visit("/");
-
-    cy.get("#header-sign-up").click({ force: true });
-
-    // cy.get("#email").click().type("asd@asd");
-    cy.get("#email").as("emailField").click({ force: true });
-    cy.get("@emailField").type("asd@asd");
-    cy.get("#first_name").click().type("Tester");
-    cy.get("#last_name").click().type("Aqa");
-    cy.get("#password").click().type("Qwe123qwe123*");
-
-    cy.get("#terms_and_conditions").click();
-    cy.get(
-      'button[class="c-kDQqQr c-kDQqQr-fwzCzT-background-light c-kDQqQr-cOvXws-cv c-ddSKil"]'
-    ).click();
-
-    cy.get(
-      "#__next > div > main > section > div.c-cUhiIV.c-cFpcyF > div > div.c-kbJYtg > div > form > div.c-UUKrH.c-UUKrH-kDyeyw-type-error"
-    );
-  });
-
-  // it("Make a transition to the All products page", () => {
+  // it("Sign up to Telnyx with not valid email", () => {
   //   cy.on("uncaught:exception", (err, runnable) => {
   //     // поверни false, щоб Cypress не зупиняв тест при виникненні цієї помилки
   //     if (
@@ -52,25 +19,44 @@ describe("The Home Page", () => {
 
   //   cy.visit("/");
 
-  //   cy.get("#radix-\\:r1\\:")
-  //     .scrollIntoView()
-  //     .should("be.visible")
-  //     .click({ force: true });
+  //   cy.get("#header-sign-up").click({ force: true });
 
   //   // cy.get("#email").click().type("asd@asd");
-  //   cy.get("div > div.c-jEQANY > a").click();
-  //   // cy.get("@emailField").type("asd@asd");
-  //   // cy.get("#first_name").click().type("Tester");
-  //   // cy.get("#last_name").click().type("Aqa");
-  //   // cy.get("#password").click().type("Qwe123qwe123*");
+  //   cy.get("#email").as("emailField").click({ force: true });
+  //   cy.get("@emailField").type("asd@asd");
+  //   cy.get("#first_name").click().type("Tester");
+  //   cy.get("#last_name").click().type("Aqa");
+  //   cy.get("#password").click().type("Qwe123qwe123*");
 
-  //   // cy.get("#terms_and_conditions").click();
-  //   // cy.get(
-  //   //   'button[class="c-kDQqQr c-kDQqQr-fwzCzT-background-light c-kDQqQr-cOvXws-cv c-ddSKil"]'
-  //   // ).click();
+  //   cy.get("#terms_and_conditions").click();
+  //   cy.get(
+  //     'button[class="c-kDQqQr c-kDQqQr-fwzCzT-background-light c-kDQqQr-cOvXws-cv c-ddSKil"]'
+  //   ).click();
 
-  //   // cy.get(
-  //   //   "#__next > div > main > section > div.c-cUhiIV.c-cFpcyF > div > div.c-kbJYtg > div > form > div.c-UUKrH.c-UUKrH-kDyeyw-type-error"
-  //   // );
+  //   cy.get(
+  //     "#__next > div > main > section > div.c-cUhiIV.c-cFpcyF > div > div.c-kbJYtg > div > form > div.c-UUKrH.c-UUKrH-kDyeyw-type-error"
+  //   );
   // });
+
+  it("Make a transition to the Solutions page", () => {
+    cy.on("uncaught:exception", (err, runnable) => {
+      // поверни false, щоб Cypress не зупиняв тест при виникненні цієї помилки
+      if (
+        err.message.includes(
+          "The fetching process for the media resource was aborted"
+        )
+      ) {
+        return false;
+      }
+    });
+
+    cy.visit("/");
+
+    cy.get('a[href="/solutions"]').click({ force: true });
+
+    cy.get('h1[class="c-PJLV c-fGbiyG c-PJLV-cHtIMp-dark-false"]').should(
+      "have.text",
+      "Solutions for scaling your business"
+    );
+  });
 });
