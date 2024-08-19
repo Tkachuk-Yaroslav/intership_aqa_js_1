20b69108a1d5a9c6699ff2dd51d4d8e5b9610f07
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-08-20 00:47:57 +0300

    added telnyx-shop specs

---
+++ cypress/e2e/telnyx-shop.cy.js
@@ -0,0 +1,58 @@
+import { setupInterceptions } from "../support/interceptors";
+
+import MainPage from "../../pages/main.page.js";
+import ShopTelnyxPage from "../../pages/shopTelnyx.page.js";
+
+describe("The Home Page", () => {
+  //test case 7
+  it("Add the product to the cart on the store page", () => {
+    setupInterceptions();
+
+    cy.visit("/");
+    MainPage.removeAttrInShopLink("target");
+    MainPage.clickOnShopLink();
+
+    ShopTelnyxPage.scrollToShopifySection();
+
+    ShopTelnyxPage.clickOnAddItemBtn();
+
+    ShopTelnyxPage.shouldCartDriwerBeVisible();
+    ShopTelnyxPage.shouldAddedItemLink([
+      "be.visible",
+      "have.text",
+      "Telnyx Classic Hat",
+    ]);
+  });
+
+  //test case 8
+  it("Delete the product from the cart on the store page", () => {
+    setupInterceptions();
+
+    cy.visit("/");
+
+    MainPage.removeAttrInShopLink("target");
+    MainPage.clickOnShopLink();
+
+    ShopTelnyxPage.scrollToShopifySection();
+
+    ShopTelnyxPage.clickOnAddItemBtn();
+
+    ShopTelnyxPage.shouldCartDriwerBeVisible();
+
+    ShopTelnyxPage.shouldAddedItemLink([
+      "be.visible",
+      "have.text",
+      "Telnyx Classic Hat",
+    ]);
+
+    ShopTelnyxPage.clickOnRemoveAddedItemBtn();
+
+    ShopTelnyxPage.shouldEmptyCartWrapBeVisible();
+
+    ShopTelnyxPage.shouldEmptyCartTitle([
+      "be.visible",
+      "have.text",
+      "Your cart is empty",
+    ]);
+  });
+});
